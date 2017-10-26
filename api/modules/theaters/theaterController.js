67f30744632f0c42fd2434f2bcc42ad43d57407c
SimonGodefroid <simon.godefroid@gmail.com>
2017-10-26 16:46:27 +0200

    added pagination to the theaters/ list query

--- api/modules/theaters/theaterController.js
+++ api/modules/theaters/theaterController.js
@@ -1,23 +1,64 @@
 (() => {
 	'use strict';
 	const Theater = require('./theaterModel'),
-		request = require('request-promise');
+		keys = Object.keys(Theater.schema.paths),
+		request = require('request-promise'),
+		mongoose = require('mongoose'),
+		_ = require('lodash');
 	module.exports = acl => {
 		return {
 			list: (req, res) => {
-				Theater.find({})
-					.exec()
-					.then(theaters => {
-						return res.status(200).json({
-							success: true,
-							count: theaters.length,
-							message: theaters
+				let query = [];
+				let skip = '';
+				let limit = '';
+				let sort = {};
+				let match = {};
+				let q = {};
+				let matchKeys = req.query ? _.without(Object.keys(req.query), 'limit', 'page', 'sort') : [];
+				if (matchKeys.length > 0) {
+					q.$and = [];
+					for (let i in matchKeys) {
+						match = {};
+						match[matchKeys[i]] = {};
+						match[matchKeys[i]]['$in'] = [];
+						let values = req['query'][matchKeys[i]].split(',');
+						values.map(val => {
+							return match[matchKeys[i]]['$in'].push(parseInt(val) || val);
 						});
-					})
-					.catch(err => {
-						console.log('err', err);
-						return res.status(500).json({ success: false, message: err });
+						q.$and.push(match);
+					}
+				}
+				!req.query.page || isNaN(req.query.page) ? (skip = 1) : (skip = parseInt(req.query.page));
+				!req.query.limit || isNaN(req.query.limit) ? (limit = 100) : (limit = parseInt(req.query.limit));
+				if (req.query.sort && (req.query.sort !== '' || Object.keys(req.query.sort).length > 0)) {
+					for (let i in Object.keys(req.query.sort)) {
+						sort[Object.keys(req.query.sort)[i]] = parseInt(Object.values(req.query.sort)[i]);
+					}
+					query.push({ $sort: sort });
+				}
+				query.push({ $match: q });
+				query.push({ $skip: (skip - 1) * limit });
+				query.push({ $limit: limit });
+				console.log(JSON.stringify(query, null, 4));
+				Theater.aggregate([{ $match: q }], (err, count) => {
+					Theater.count({}, (err, total) => {
+						Theater.aggregate(query, (err, theaters) => {
+							if (err) {
+								console.log('An error occurred' + err);
+								return res.status(500).json({ success: false, message: err });
+							} else {
+								res.status(200).json({
+									success: true,
+									limit: limit,
+									total: total,
+									results: count.length,
+									count: theaters.length,
+									message: theaters
+								});
+							}
+						});
 					});
+				});
 			},
 			read: (req, res) => {
 				let query = [{ $match: { _id: mongoose.Types.ObjectId(req.params.id) } }];
