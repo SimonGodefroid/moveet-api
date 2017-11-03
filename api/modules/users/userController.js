d94f74035e06993e4a0ac8128570c9fa04bbfb4f
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-03 14:32:59 +0100

    added query builder to userController

--- api/modules/users/userController.js
+++ api/modules/users/userController.js
@@ -2,24 +2,93 @@
 	'use strict';
 	const User = require('./userModel'),
 		Movie = require('../movies/movieModel.js'),
-		mongoose = require('mongoose');
+		mongoose = require('mongoose'),
+		keys = Object.keys(User.schema.paths),
+		_ = require('lodash');
 	// Global = require('../../../Global');
 	module.exports = acl => {
 		return {
+			// list: (req, res) => {
+			// 	User.find({})
+			// 		.exec()
+			// 		.then(users => {
+			// 			return res.status(200).json({
+			// 				success: true,
+			// 				count: users.length,
+			// 				message: users
+			// 			});
+			// 		})
+			// 		.catch(err => {
+			// 			console.log('err', err);
+			// 			return res.status(500).json({ success: false, message: err });
+			// 		});
 			list: (req, res) => {
-				User.find({})
-					.exec()
-					.then(users => {
-						return res.status(200).json({
-							success: true,
-							count: users.length,
-							message: users
+				let query = [];
+				let skip = '';
+				let limit = '';
+				let sort = {};
+				let match = {};
+				let q = {};
+				let matchKeys = req.query ? _.without(Object.keys(req.query), 'limit', 'page', 'sort') : [];
+				if (matchKeys.length > 1) {
+					q.$and = [];
+					for (let i in matchKeys) {
+						match = {};
+						if (req['query'][matchKeys[i]] !== '') {
+							match[matchKeys[i]] = {};
+							match[matchKeys[i]]['$in'] = [];
+							let values = req['query'][matchKeys[i]].split(',');
+							values.map(val => {
+								return match[matchKeys[i]]['$in'].push(parseInt(val) || val);
+							});
+							q.$and.push(match);
+						}
+					}
+				} else if (matchKeys.length === 1) {
+					if (req['query'][matchKeys[0]] !== '') {
+						q[matchKeys[0]] = {};
+						q[matchKeys[0]]['$in'] = [];
+						let values = req['query'][matchKeys[0]].split(',');
+						values.map(val => {
+							return q[matchKeys[0]]['$in'].push(parseInt(val) || val);
 						});
-					})
-					.catch(err => {
-						console.log('err', err);
-						return res.status(500).json({ success: false, message: err });
+						// console.log('q', q);
+					}
+				}
+				!req.query.page || isNaN(req.query.page) ? (skip = 1) : (skip = parseInt(req.query.page));
+				!req.query.limit || isNaN(req.query.limit) ? (limit = 100) : (limit = parseInt(req.query.limit));
+
+				if (req.query.sort && (req.query.sort !== '' || Object.keys(req.query.sort).length > 0)) {
+					for (let i in Object.keys(req.query.sort)) {
+						// sort[Object.keys(req.query.sort)[i]] = parseInt(Object.values(req.query.sort)[i]);
+						sort[Object.keys(req.query.sort)[i]] = parseInt(_.values(req.query.sort)[i]);
+					}
+					// console.log('sort', sort);
+					query.push({ $sort: sort });
+				}
+				query.push({ $match: q });
+				query.push({ $skip: (skip - 1) * limit });
+				query.push({ $limit: limit });
+				console.log(JSON.stringify(query, null, 4));
+				User.aggregate([{ $match: q }], (err, count) => {
+					User.count({}, (err, total) => {
+						User.aggregate(query, (err, users) => {
+							if (err) {
+								console.log('An error occurred' + err);
+								return res.status(500).json({ success: false, message: err });
+							} else {
+								res.status(200).json({
+									success: true,
+									limit: limit,
+									total: total,
+									results: count.length,
+									count: users.length,
+									message: users
+								});
+							}
+						});
 					});
+				});
 			},
 			read: (req, res) => {
 				let query = [{ $match: { _id: mongoose.Types.ObjectId(req.params.id) } }];
